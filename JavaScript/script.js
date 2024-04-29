7956c50f1fe5287e8012c9a49afe177de5bc4105
turanaskerli <157804358+turanaskerli@users.noreply.github.com>
2024-04-29 00:44:07 -0700

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,47 @@
+// First taskW
+
+// let x = +prompt("Enter first number: ");
+
+// let y = +prompt("Enter second number: ");
+
+// console.log(x - y);
+
+// Second task
+
+// let x = "hello";
+
+// let y = "world";
+
+// alert(x + "-" + y);
+
+// Third task
+
+// let x = 5;
+
+// let y = "five";
+
+//  alert(x + y);
+
+// Fourth task
+
+// let x = 15
+
+// let y = 25
+
+// console.log(x + y);
+
+// Fifth task
+
+// let x = 5;
+
+// console.log(x*x);
+
+// Sixth task
+
+// let x = +prompt("Enter first number: ");
+
+// let y = +prompt("Enter second number: ");
+
+// let z = +prompt("Enter third number: ");
+
+// console.log((x + y + z) / 3);
